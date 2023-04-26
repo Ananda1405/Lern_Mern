7c0dd1f650e4a879d1316b9995ae0d3dfdc50526
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-04-27 00:22:24 +0600

    Rest API Basic
    Doing Rest API Basic

---
+++ backend/api/routes/main.routes.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const mainController = require('../controllers/main.controller');
+
+//HTTP Verbs: POST, GET, PUT, DELETE
+
+//POST /api/providers
+router.post('/providers', mainController.create);
+
+//GET /api/providers
+router.get('/providers',mainController.readAll);
+
+//GET one /api/providers/123
+router.get('/providers/:id',mainController.readOne);
+
+//PUT /api/providers/123
+router.put('/providers',mainController.update);
+
+//Delete one provider /api/providers/123
+router.delete('/providers/:id',mainController,deleteOne);
+
+//Delete all providers /api/providers
+router.delete('/providers',mainController.deleteAll);
+
+module.exports = router;
