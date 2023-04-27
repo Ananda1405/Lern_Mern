0fb653915cbe76e1f2f1ce05bf2ec88706172a0b
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-04-27 12:19:01 +0600

    Refactoring REST API

--- backend/api/controllers/main.controller.js
+++ backend/api/controllers/main.controller.js
@@ -1,35 +1,69 @@
 var providers = require('../models/providers.models');
 
+//Utill functions
+//Check if list is empty
+function isEmptyList(obj){
+    return (~obj || obj.length == 0 || Object.keys(obj).length == 0);
+}
+
+//Check for existing provider
+function existsProvider(id){
+    return providers.find(provider => provider.id == id);
+}
+
+//Generate a unique provider id
+function getUniqueId(providers){
+    //Create Random ID
+    let min = 100000;
+    let max = 999999;
+    do{
+        var id = Math.floor(Math.random() * (max-min) + min);
+    }while(existsProvider(id));
+
+    return id;
+}
+
+
 //CRUD - Create (Post), Read (Get), Update (PUT), Delete
 
 //post
 //uri: /api/providers
 module.exports.create = function(req, res){
-        //Create Random ID
-        let min = 100000;
-        let max = 999999;
-        let id = Math.floor(Math.random() * (max-min) + min);
+        // Create Random ID
+        if(isEmptyList(providers)){
+            providers = [];
+        }
+
+        var id = req.body.id;
+        if(existsProvider(id)){
+            res.status(400);
+            res.send('Duplicate id not allowed.:(');
+            id = getUniqueId(); //get new ID
+        }
+
+        var provider = req.body; //get new provider
+        provider.id = id;
     
         //create new provider object
     
-        let provider = {
-            id:id,
-            firstname : req.body.firstname,
-            lastname : req.body.lastname,
-            position : req.body.position,
-            company : {
-                company_name : req.body.company.company_name,
-                address : req.body.company.address,
-                address2 : req.body.company.address2,
-                city : req.body.company.city,
-                state : req.body.company.state,
-                postal_code : req.body.company.postal_code,
-                phone : req.body.company.phone,
-                email : req.body.company.email,
-                description : req.body.company.description,
-                tagline : req.body.company.tagline,
-            }
-        }
+        // let provider = {
+        //     id:id,
+        //     firstname : req.body.firstname,
+        //     lastname : req.body.lastname,
+        //     position : req.body.position,
+        //     company : {
+        //         company_name : req.body.company.company_name,
+        //         address : req.body.company.address,
+        //         address2 : req.body.company.address2,
+        //         city : req.body.company.city,
+        //         state : req.body.company.state,
+        //         postal_code : req.body.company.postal_code,
+        //         phone : req.body.company.phone,
+        //         email : req.body.company.email,
+        //         description : req.body.company.description,
+        //         tagline : req.body.company.tagline,
+        //     }
+        // }
         //Add New Provider to The List
         providers.push(provider);
         res.status(200);
@@ -39,6 +73,10 @@ module.exports.create = function(req, res){
 //GET All
 //uri: /api/providers
 module.exports.readAll = function(req, res){
+    if(isEmptyList(providers)){
+        res.status(404);
+        res.send('List is empty.');
+    }
     res.status(200);
     res.send(providers);
 }
@@ -46,6 +84,10 @@ module.exports.readAll = function(req, res){
 //GET One
 //uri: /api/providers/123
 module.exports.readOne = function(req, res){
+    if(isEmptyList(providers)){
+        res.status(404);
+        res.send('List is empty.');
+    }
     let id = req.params.id;
     let provider = providers.find( provider=> provider.id == id )
     res.status(200);
@@ -55,7 +97,11 @@ module.exports.readOne = function(req, res){
 //PUT 
 //uri: /api/providers/123
 module.exports.update = function(req, res){
-    module.exports.update = function(req, res){
+       
+        if(isEmptyList(providers)){
+            res.status(404);
+            res.send('List is empty.Cannot Update');
+        }
         let id = req.params.id;
         let provider = providers.find( provider=> provider.id == id );
         provider.firstname = req.body.firstname;
@@ -74,13 +120,17 @@ module.exports.update = function(req, res){
     
         res.status(200);
         res.send(provider);
-    }
+    
 }
 
 //Delete One 
 //uri: /api/providers/123
 module.exports.deleteOne = function(req, res){
-    module.exports.delete = function(req, res){
+    
+    if(isEmptyList(providers)){
+        res.status(404);
+        res.send('List is empty. Cannot Delete');
+    }
         let id = req.params.id;
         let provider = providers.find( provider=> provider.id == id )
         let idx = providers.indexOf(providers.find( provider=> provider.id == id ));
@@ -89,12 +139,16 @@ module.exports.deleteOne = function(req, res){
     
         res.status(200); 
         res.send(provider);
-    }
+    
 }
 
 //Delete All 
 //uri: /api/providers
 module.exports.deleteAll = function(req,res){
+    if(isEmptyList(providers)){
+        res.status(404);
+        res.send('List is empty.CAnt delete');
+    }
     providers = [];
     res.status(200);
     res.send("All Providers Deleted");
